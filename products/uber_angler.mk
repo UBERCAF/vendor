ffcf35e9a90a08e191c902d1a6741a98542ff567
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-09-25 20:23:50 +0200

    Angler: set BOARD_USES_QCOM_HARDWARE to true

--- products/uber_angler.mk
+++ products/uber_angler.mk
@@ -4,7 +4,46 @@ $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/uber/config/common.mk)
 
+BOARD_USES_QCOM_HARDWARE := true
 
+# Use kernel headers for some HALs
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_HAS_QC_KERNEL_SOURCE := true
+
+# CAF display addons
+PRODUCT_PACKAGES += \
+    copybit.msm8994
+
+# MSM8994 Display HAL settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
+    debug.sf.hw=1 \
+    persist.debug.wfd.enable=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
+    persist.mdpcomp.4k2kSplit=1 \
+    persist.hwc.mdpcomp.maxpermixer=5 \
+    persist.mdpcomp_perfhint=50 \
+    persist.metadata_dynfps.disable=true \
+    persist.sys.wfd.virtual=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.HAL3.enabled=1 \
+    persist.camera.cpp.duplication=false \
+    media.camera.ts.monotonic=0
+
+# Skip boot jars check
+SKIP_BOOT_JARS_CHECK := true
+
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
+# ROI (region of interest) is miscalculated right now
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.mdp.partialUpdate=false
 
 # Setup device specific product configuration.
 PRODUCT_NAME := uber_angler
@@ -21,4 +60,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Boot animation
 PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
-
